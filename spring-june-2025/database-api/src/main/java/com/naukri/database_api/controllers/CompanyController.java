f929ab9baef98e78ef8a6c4e7ef0102c9446f5bb
Tab1999 <tabassumchhalwadi1999@gmail.com>
2025-06-16 15:49:11 +0530

    HW Done

--- spring-june-2025/database-api/src/main/java/com/naukri/database_api/controllers/CompanyController.java
+++ spring-june-2025/database-api/src/main/java/com/naukri/database_api/controllers/CompanyController.java
@@ -1,3 +1,4 @@
+
 package com.naukri.database_api.controllers;
 
 import com.naukri.database_api.models.Company;
@@ -5,25 +6,42 @@ import com.naukri.database_api.repositories.CompanyRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/db")
+@RequestMapping("/api/v1/db/company")
 public class CompanyController {
 
-    CompanyRepo companyrepo;
+    CompanyRepo companyRepository;
 
     @Autowired
-    public void CompanyRepo(CompanyRepo companyrepo){
-        this.companyrepo = companyrepo;
+    public CompanyController(CompanyRepo companyRepository) {
+        this.companyRepository = companyRepository;
     }
 
     @PostMapping("/save")
-    public ResponseEntity createCompany(@RequestBody Company company){
-        companyrepo.save(company);
-        return new ResponseEntity(company,HttpStatus.CREATED);
+    public ResponseEntity createCompany(@RequestBody Company company) {
+        companyRepository.save(company);
+        return new ResponseEntity(company, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getCompanyById(@PathVariable UUID id) {
+        Company company = companyRepository.findById(id).orElse(null);
+        return new ResponseEntity<>(company, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity updateCompany(@RequestBody Company company) {
+        companyRepository.save(company);
+        return new ResponseEntity(company, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteCompanyById(@PathVariable UUID id) {
+        companyRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
